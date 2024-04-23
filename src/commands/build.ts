5c22cb29ef22592b58533c84616c87a4bf7db55c
Nishida Michiya <m.qrowd@gmail.com>
2024-04-23 23:44:24 +0900

    feat: buildコマンド追加

---
+++ src/commands/build.ts
@@ -0,0 +1,60 @@
+import * as fs from 'fs';
+import KintonePlugin from '@kintone/webpack-plugin-kintone-plugin';
+import * as uploader from '@kintone/plugin-uploader';
+import { defineCommand } from "citty";
+import consola from "consola";
+import webpack from "webpack";
+import { defineWebpackConfig } from "../config/webpack.config.js";
+import { pluginZipFilePath } from '../utils/zipPath.js';
+import { resolve } from 'pathe';
+
+export default defineCommand({
+  meta: {
+    name: 'build',
+    description: 'Upload plugin for production',
+  },
+  async run() {
+    consola.info('Upload plugin for production...');
+
+    const zipFilePath = await pluginZipFilePath(process.cwd());
+    const kintonePlugin = new KintonePlugin({
+      manifestJSONPath: resolve(process.cwd(), 'plugin/manifest.json'),
+      privateKeyPath: resolve(process.cwd(), 'private.ppk'),
+      pluginZipPath: zipFilePath,
+    });
+    const config = defineWebpackConfig(process.cwd(), kintonePlugin);
+
+    // .envファイルが存在しない場合は作成する
+    await generateDotEnv();
+    const env = (await import('dotenv')).config().parsed;
+    if(!env) {
+      consola.error('Failed to upload plugin for development');
+      process.exit(1);
+    }
+
+    webpack({ ...config, mode: 'production' }, (err, stats) => {
+      if (err || stats?.hasErrors()) {
+        consola.error('Failed to upload plugin for development');
+        return;
+      }
+      consola.success('Successfully uploaded plugin for development');
+      uploader.run(`https://${env.KINTONE_DOMAIN}.cybozu.com`, env.KINTONE_USERNAME, env.KINTONE_PASSWORD, zipFilePath, { lang: 'ja'});
+    });
+  }
+});
+
+async function generateDotEnv() {
+  if (!fs.existsSync(resolve(process.cwd(), '.env'))) {
+    consola.info('Please Tell me your kintone domain, username, and password.');
+    // domain, username, passwordを入力してもらう
+    consola.info('Please input your kintone domain.');
+    const domain = await consola.prompt('Domain:');
+    consola.info('Please input your kintone username.');
+    const username = await consola.prompt('Username:');
+    consola.info('Please input your kintone password.');
+    // password は入力時に表示されないようにする
+    const password = await consola.prompt('Password:');
+    fs.writeFileSync(resolve(process.cwd(), '.env'), `KINTONE_DOMAIN=${domain}\nKINTONE_USERNAME=${username}\nKINTONE_PASSWORD=${password}`);
+    consola.success('Successfully created .env file');
+  }  
+}
--- src/commands/index.ts
+++ src/commands/index.ts
@@ -5,4 +5,5 @@ const _rDefault = (r: any) => (r.default || r) as Promise<CommandDef>;
 export const commands = {
   init: () => import('./init.js').then(_rDefault),
   dev: () => import('./dev.js').then(_rDefault),
+  build: () => import('./build.js').then(_rDefault),
 } as const;
